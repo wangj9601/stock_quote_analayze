b7eed6238b315bd86aa539b78832db0bd3660ae9
wangj9601 <wangxuewu@sina.com>
2025-05-30 16:09:27 +0800

    本地更新：同步所有最新更改

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,6 +1,4 @@
-
-const API_BASE_URL = window.API_BASE_URL || 'http://localhost:5000';
-
+//const API_BASE_URL = window.API_BASE_URL || 'http://192.168.31.237:5000';
 
 // 首页数据管理
 document.addEventListener('DOMContentLoaded', function() {
