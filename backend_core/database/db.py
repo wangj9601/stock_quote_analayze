bb8ca4a42e667aec4d5f186f432e0ad1f0e80a90
wangj9601 <wangxuewu@sina.com>
2025-07-16 09:55:24 +0800

    本地更新：同步所有最新更改 20250716

--- backend_core/database/db.py
+++ backend_core/database/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-DATABASE_URL = "postgresql+psycopg2://postgres:qidianspacetime@localhost:5446/stock_analysis"
+DATABASE_URL = "postgresql+psycopg2://postgres:qidianspacetime@192.168.31.237:5446/stock_analysis"
 
 engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
